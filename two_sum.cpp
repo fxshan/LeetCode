4d0386095ce1361ab9b27248cd8ca1b580287aa7
fxshan <fengxiaoshan.fr@gmail.com>
2020-05-26 22:52:08 +0200

    Two Sum

---
+++ two_sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+      if (mp.count(nums[i])) return {mp[nums[i]], i};
+      mp[target - nums[i]] = i;
+    }
+    return {0, 0};
+  }
+};
