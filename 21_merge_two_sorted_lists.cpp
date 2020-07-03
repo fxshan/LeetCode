d70e5444e7984f24aed34194c513d0cbaa176902
Xiaoshan Feng <fengxiaoshan.fr@gmail.com>
2020-07-03 19:53:29 +0200

    better solution for ListNode

--- 21_merge_two_sorted_lists.cpp
+++ 21_merge_two_sorted_lists.cpp
@@ -20,8 +20,8 @@ class Solution {
         cur->next = l2, l2 = l2->next;
       cur = cur->next;
     }
-    while (l1) cur->next = l1, l1 = l1->next, cur = cur->next;
-    while (l2) cur->next = l2, l2 = l2->next, cur = cur->next;
+    if (l1) cur->next = l1;
+    if (l2) cur->next = l2;
     return dummy->next;
   }
 };
