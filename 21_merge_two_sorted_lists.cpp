5807686e61eb6080793bc6c3c86c8f52e3edd528
fxshan <fengxiaoshan.fr@gmail.com>
2020-07-03 19:38:27 +0200

    Merge two sorted lists

---
+++ 21_merge_two_sorted_lists.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     nt val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* cur = dummy;
+    while (l1 && l2) {
+      if (l1->val < l2->val)
+        cur->next = l1, l1 = l1->next;
+      else
+        cur->next = l2, l2 = l2->next;
+      cur = cur->next;
+    }
+    while (l1) cur->next = l1, l1 = l1->next, cur = cur->next;
+    while (l2) cur->next = l2, l2 = l2->next, cur = cur->next;
+    return dummy->next;
+  }
+};
