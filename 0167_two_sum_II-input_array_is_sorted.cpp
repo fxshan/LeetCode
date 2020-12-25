be9ae5378f5f5de80529638ef1aa89b69f71fefd
Xiaoshan Feng <fengxiaoshan.fr@gmail.com>
2020-12-25 22:16:09 +0100

    Two Sum II - Input array is sorted

---
+++ 0167_two_sum_II-input_array_is_sorted.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> mp;
+    for (int i = 0; i < nums.size(); i++) {
+      if (mp.count(nums[i])) return {mp[nums[i]] + 1, i + 1};
+      mp[target - nums[i]] = i;
+    };
+    return {};
+  }
+};
