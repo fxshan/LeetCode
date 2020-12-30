ce2eb31aaac59230166d577e4f66242bcf92cafa
Xiaoshan Feng <fengxiaoshan.fr@gmail.com>
2020-12-30 15:38:07 +0100

    Excel Sheet Column Number - better solution

--- 0171_excel_sheet_column_number.cpp
+++ 0171_excel_sheet_column_number.cpp
@@ -1,13 +1,10 @@
 class Solution {
  public:
   int titleToNumber(string s) {
-    string str = s;
-    reverse(str.begin(), str.end());
-    int r = 0, n = 0;
-    for (char c : str) {
-      r += ((int)c - 64) * pow(26, n);
-      n++;
+    int n = s.size(), res = 0;
+    for (int i = 0; i < n; i++) {
+      res += int(s[i] - 'A' + 1) * pow(26, n - i - 1);
     };
-    return r;
+    return res;
   }
 };
