6da87257cc828ad8261539e22bc273fa0231a67c
fxshan <fengxiaoshan.fr@gmail.com>
2021-01-10 22:53:45 +0100

    Contains Duplicate

---
+++ 0217_contains_duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> set;
+    for (int num : nums) {
+      set.insert(num);
+    }
+    return set.size() != nums.size();
+  }
+};
