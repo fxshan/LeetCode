73076f0d10d1fae020c6f80453473792f229b341
fxshan <fengxiaoshan.fr@gmail.com>
2021-03-14 22:25:31 +0100

    Is Subsequence

--- 0392_is_subsequence.cpp
+++ 0392_is_subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
  public:
   bool isSubsequence(string s, string t) {
     int m = s.size(), n = t.size(), ind = 0;
-    for (int i = 0; i < t.size(); i++)
+    for (int i = 0; i < n; i++)
       if (s[ind] == t[i]) ind++;
     return m == ind;
   }
