cfad852bb16d6ac4733a8e267de2976bbed2a58e
Xiaoshan Feng <fengxiaoshan.fr@gmail.com>
2021-05-31 10:47:02 +0200

    Set Mismatch improved

--- 0645_set_mismatch.cpp
+++ 0645_set_mismatch.cpp
@@ -2,17 +2,12 @@ class Solution {
  public:
   vector<int> findErrorNums(vector<int>& nums) {
     sort(nums.begin(), nums.end());
-    vector<int> res;
     int dup = -1, missing = 1;
     for (int i = 1; i < nums.size(); i++) {
       if (nums[i] > nums[i - 1] + 1) missing = nums[i - 1] + 1;
       if (nums[i] == nums[i - 1]) dup = nums[i];
     }
-    res.push_back(dup);
-    if (nums[nums.size() - 1] != nums.size())
-      res.push_back(nums.size());
-    else
-      res.push_back(missing);
-    return res;
+    if (nums[nums.size() - 1] != nums.size()) missing = nums.size();
+    return {dup, missing};
   }
 };
