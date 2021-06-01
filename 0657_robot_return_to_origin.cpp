08a9c3ecf9a30081c7add50c000180427549f7b3
Xiaoshan Feng <fengxiaoshan.fr@gmail.com>
2021-06-01 19:51:49 +0200

    Robot Return to Origin

---
+++ 0657_robot_return_to_origin.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  bool judgeCircle(string moves) {
+    int v = 0, h = 0;
+    for (char c : moves) {
+      switch (c) {
+        case 'U':
+          v++;
+          break;
+        case 'D':
+          v--;
+          break;
+        case 'R':
+          h++;
+          break;
+        case 'L':
+          h--;
+          break;
+      }
+    }
+    return v == 0 && h == 0;
+  }
+};
