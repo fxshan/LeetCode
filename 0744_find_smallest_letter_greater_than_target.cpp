b171d4c4db3924cfa3b7de22ad95a26ca693fc49
Xiaoshan Feng <fengxiaoshan.fr@gmail.com>
2021-07-30 22:08:53 +0200

    Find Smallest Letter Greater Than Target

---
+++ 0744_find_smallest_letter_greater_than_target.cpp
@@ -0,0 +1,9 @@
+class Solution {
+ public:
+  char nextGreatestLetter(vector<char>& letters, char target) {
+    for (int i = 0; i < letters.size(); i++) {
+      if ((int)target < (int)letters[i]) return letters[i];
+    }
+    return letters[0];
+  }
+};
